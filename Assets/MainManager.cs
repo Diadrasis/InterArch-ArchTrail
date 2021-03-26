2f50ebc42c0f3bb99c56c72f3bc124a23c0e30cc
emmanouela88 <76942908+emmanouela88@users.noreply.github.com>
2021-03-26 17:29:39 +0200

    Android PermissionsDone and check for gps

--- Assets/MainManager.cs
+++ Assets/MainManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Stathis.Android;
 using UnityEngine.Android;
 using UnityEngine.UI;
 
@@ -9,7 +10,7 @@ public class MainManager : MonoBehaviour
 {
     OnlineMaps instance;
     private OnlineMapsMarker playerMarker;
-    public Button btnLayer, btnCurrentLoc;
+    public Button btnLayer, btnCurrentLoc, btnGPS;
     public Text infoText;
     public float time = 3;
     private float angle;
@@ -20,29 +21,24 @@ public class MainManager : MonoBehaviour
     private double fromTileX, fromTileY, toTileX, toTileY;
     private int moveZoom;
     OnlineMapsLocationService locationService;
+    //public GameObject blackScreen;
 
+    private void Awake()
+    {
 
+        Application.runInBackground = true;
+
+    }
     void Start()
     {
-        locationService = OnlineMapsLocationService.instance;
+        
+        btnGPS.onClick.AddListener(() => OpenNativeAndroidSettings());
+        InitLocation();
         //StartCoroutine(GetStarting());
-        btnLayer.onClick.AddListener(() => CheckLayer());
-        btnCurrentLoc.onClick.AddListener(() => CheckMyLocation());
-#if PLATFORM_ANDROID
-        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
-        {
-            Permission.RequestUserPermission(Permission.FineLocation);
-            if (Permission.HasUserAuthorizedPermission(Permission.FineLocation))
-            {
-                if (locationService.IsLocationServiceRunning())
-                    locationService.StartLocationService();
-                else
-                {
-                    StartCoroutine(OpenSettings());
-                }
-            }
-
-        }
+    }
+    void InitLocation()
+    {
+        locationService = OnlineMapsLocationService.instance;
         playerMarker = OnlineMapsMarkerManager.CreateItem(new Vector2(0, 0), null, "Player");
 
         if (locationService == null)
@@ -54,66 +50,93 @@ public class MainManager : MonoBehaviour
 
         // Subscribe to the change location event.
         locationService.OnLocationChanged += OnLocationChanged;
-        //locationService.OnGetLocation = 
 
+        if (CheckForLocationServices()) return;
+    }
+
+    private bool CheckForLocationServices()
+    {
+        if (locationService == null) return false;
+
+#if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+            if (Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+            {
+                locationService.StartLocationService();
+                locationService.IsLocationServiceRunning();
+            }
+        }
 #endif
+        if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor)
+        {
+            if (locationService.useGPSEmulator)
+
+            {
+                Debug.Log(locationService);
+                infoText.text = "Checking location";
+                StartCoroutine(CheckAppLocation());
+                return true;
+            }
+            else
+            {
+                infoText.text = "Press the gps button";
+                StartCoroutine(CheckAppLocation());
+                return true;
+            }
+            
+        }
+        return false;
     }
 
     /* IEnumerator GetStarting()
-     {
+{
 
- #if UNITY_EDITOR
-         int editorMaxWait = 15;
-         while (Input.location.status == LocationServiceStatus.Stopped && editorMaxWait > 0)
-         {
-             yield return new WaitForSecondsRealtime(1);
-             editorMaxWait--;
-         }
- #endif
-         // Start service before querying location
-         Input.location.Start();
-         if (!Input.location.isEnabledByUser)
-             yield break;
+#if UNITY_EDITOR
+    int editorMaxWait = 15;
+    while (Input.location.status == LocationServiceStatus.Stopped && editorMaxWait > 0)
+    {
+        yield return new WaitForSecondsRealtime(1);
+        editorMaxWait--;
+    }
+#endif
+    // Start service before querying location
+    Input.location.Start();
+    if (!Input.location.isEnabledByUser)
+        yield break;
 
-         // Wait until service initializes
-         int maxWait = 20;
-         while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-         {
-             yield return new WaitForSeconds(1);
-             maxWait--;
-         }
+    // Wait until service initializes
+    int maxWait = 20;
+    while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+    {
+        yield return new WaitForSeconds(1);
+        maxWait--;
+    }
 
-         // Service didn't initialize in 20 seconds
-         if (maxWait < 1)
-         {
-             print("Timed out");
-             yield break;
-         }
+    // Service didn't initialize in 20 seconds
+    if (maxWait < 1)
+    {
+        print("Timed out");
+        yield break;
+    }
 
-         // Connection has failed
-         if (Input.location.status == LocationServiceStatus.Failed)
-         {
-             infoText.text = "Can't get location";
-             print("Unable to determine device location");
-             yield break;
-         }
-         else
-         {
-             print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
-         }
+    // Connection has failed
+    if (Input.location.status == LocationServiceStatus.Failed)
+    {
+        infoText.text = "Can't get location";
+        print("Unable to determine device location");
+        yield break;
+    }
+    else
+    {
+        print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+    }
 
-         Input.location.Stop();
-     }*/
+    Input.location.Stop();
+}*/
     void Update()
     {
-        /*#if PLATFORM_ANDROID
-                if (Permission.HasUserAuthorizedPermission(Permission.FineLocation) && !locationService.IsLocationServiceRunning())
-                {
-                    infoText.text = "Authorize location";
-                    StartCoroutine(OpenSettings());
-                }
-        #endif
-        */
         if (!isMovement) return;
 
         // update relative position
@@ -133,32 +156,42 @@ public class MainManager : MonoBehaviour
         OnlineMaps.instance.SetPosition(px, py);
         infoText.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
     }
-    IEnumerator OpenSettings()
+   /* IEnumerator OpenSettings()
     {
         yield return new WaitForSeconds(1f);
-        try
+#if PLATFORM_ANDROID
+        if (Permission.HasUserAuthorizedPermission(Permission.FineLocation))
         {
-            using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-            using (AndroidJavaObject currentActivityObject = unityClass.GetStatic<AndroidJavaObject>("currentActivity"))
+            Permission.RequestUserPermission(Permission.FineLocation);
+            if (Permission.HasUserAuthorizedPermission(Permission.FineLocation) && !locationService.IsLocationServiceRunning())
             {
-                string packageName = currentActivityObject.Call<string>("getPackageName");
-
-                using (var uriClass = new AndroidJavaClass("android.net.Uri"))
-                using (AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromParts", "package", packageName, null))
-                using (var intentObject = new AndroidJavaObject("android.content.Intent", "android.settings.ACTION_LOCATION_SOURCE_SETTINGS", uriObject))
+                try
                 {
-                    intentObject.Call<AndroidJavaObject>("addCategory", "android.intent.category.DEFAULT");
-                    intentObject.Call<AndroidJavaObject>("setFlags", 0x10000000);
-                    currentActivityObject.Call("startActivity", intentObject);
+                    using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+                    using (AndroidJavaObject currentActivityObject = unityClass.GetStatic<AndroidJavaObject>("currentActivity"))
+                    {
+                        *//*string packageName = currentActivityObject.Call<string>("getPackageName");
+
+                        using (var uriClass = new AndroidJavaClass("android.net.Uri"))
+                        using (AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("fromParts", "package", packageName, null))*//*
+                        using (var intentObject = new AndroidJavaObject("android.content.Intent", "android.settings.ACTION_LOCATION_SOURCE_SETTINGS"*//*, uriObject*//*))
+                        {
+                            *//*intentObject.Call<AndroidJavaObject>("addCategory", "android.intent.category.DEFAULT");
+                            intentObject.Call<AndroidJavaObject>("setFlags", 0x10000000);*//*
+                            currentActivityObject.Call("startActivity", intentObject);
+                        }
+                    }
+#endif
                 }
+                catch (Exception ex)
+                {
+                    Debug.LogException(ex);
+                }
+
             }
         }
-        catch (Exception ex)
-        {
-            Debug.LogException(ex);
-        }
         yield break;
-    }
+    }*/
     void CheckMyLocation()
     {
         fromPosition = OnlineMaps.instance.position;
@@ -211,5 +244,39 @@ public class MainManager : MonoBehaviour
 
         // Redraw map.
         OnlineMaps.instance.Redraw();
+    }
+
+    IEnumerator CheckAppLocation()
+    {
+        //Vector2 currentPos = new Vector2(37.98f, 23.72413f);
+        float distance = OnlineMapsUtils.DistanceBetweenPoints(OnlineMaps.instance.position,new Vector2(38f, 23.72413f)).magnitude;
+        if (distance <= 100)
+        {
+            infoText.text = "You are in the correct area";
+            btnLayer.onClick.AddListener(() => CheckLayer());
+            btnCurrentLoc.onClick.AddListener(() => CheckMyLocation());
+            Debug.Log("Accuracy"+Input.location.lastData.horizontalAccuracy);
+            Debug.Log("Long " + Input.location.lastData.longitude + " Lat: " + Input.location.lastData.latitude);
+            Debug.Log(distance);
+            Debug.Log(locationService.desiredAccuracy);
+        }
+        else
+        {
+            infoText.text = "Please go near the area of GPS Location";
+            locationService.StopLocationService();
+            btnLayer.onClick.RemoveListener(() => CheckLayer());
+            btnCurrentLoc.onClick.RemoveListener(() => CheckMyLocation());
+        }
+        yield break;
+    }
+
+    public void OpenNativeAndroidSettings()
+    {
+        AndroidBridge.OpenIntent(IntentNames.GPS_SETTINGS);
+    }
+
+    private void OnApplicationQuit()
+    {
+        locationService.StopLocationService();
     }
 }
