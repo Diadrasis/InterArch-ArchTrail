cc7566245ed9e27d29c16f172a3e87eb9dc43082
Andrew Xeroudakis <a.xeroudakis@gmail.com>
2021-04-14 17:19:18 +0300

    Update

---
+++ Assets/Andrew/Scripts/cRoutePointAndrew.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cRoutePointAndrew
+{
+    Vector2 position;
+    float time;
+
+    public cRoutePointAndrew(Vector2 _position, float _time)
+    {
+        position = _position;
+        time = _time;
+    }
+
+    //create route points when position or time is updated
+    /*void Create()
+    {
+
+    }*/
+
+    //save each point in a route
+    /*void Save()
+    {
+
+    }*/
+
+    //delete a route point
+    void Delete()
+    {
+
+    }
+
+    //show/load route points in a route. Probable will be associated with cRoute
+    void Show()
+    {
+
+    }
+}
