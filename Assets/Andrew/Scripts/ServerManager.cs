46697339d753efe5bb3133530596f0b1c2bff475
Andrew Xeroudakis <a.xeroudakis@gmail.com>
2021-05-24 10:13:28 +0300

    Update

--- Assets/Andrew/Scripts/ServerManager.cs
+++ Assets/Andrew/Scripts/ServerManager.cs
@@ -32,10 +32,10 @@ public class ServerManager : MonoBehaviour
         postUserData = false;
 
         // Delete all areas from server
-        /*for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++)
         {
             DeleteAreaFromServer(i);
-        }*/
+        }
 
         // Delete all areas from server
         /*for (int i = 0; i < 10; i++)
@@ -119,108 +119,6 @@ public class ServerManager : MonoBehaviour
         yield return www.SendWebRequest();
         AppManager.Instance.uIManager.infoText.text = www.downloadHandler.text;
     }
-
-    /*public IEnumerator UploadFileData(string areaKey)
-    {
-        List<IMultipartFormSection> wwwForm = new List<IMultipartFormSection>();
-        areaKey = AppManager.Instance.mapManager.currentArea.ToString();
-        //if we change correctly the areakey string variable when the button that is assigned with this method, is pressed, then the xml file(in the server) will be updated
-        wwwForm.Add(new MultipartFormDataSection("areaKey",areaKey));
-
-        UnityWebRequest www = UnityWebRequest.Post(postUrl, wwwForm);
-
-        yield return www.SendWebRequest();
-
-        if(www.isHttpError || www.isNetworkError)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            Debug.Log("File has uploaded");
-        }
-    }*/
-    //from Stathis
-    /*public void UploadPathsToServer()
-    {
-        Debug.Log("UPLOADING....");
-        //pathManager.ShowMessage("UPLOADING....", 2f);
-        StopAllCoroutines();
-        StartCoroutine(PostSavedPaths());
-    }
-
-    IEnumerator PostSavedPaths()
-    {
-        List<string> savedPathsList = new List<string>();
-        Debug.Log(savedPathsList);
-        if (PlayerPrefs.HasKey(cPath.ID))
-        {
-            savedPathsList = PlayerPrefsX.GetStringArray(cPath.ID).ToList();
-            Debug.Log(savedPathsList);
-        }
-        else
-        {
-            yield break;
-        }
-        for (int i = 0; i < savedPathsList.Count; i++)
-        {
-
-            if (savedPathsList[i].Contains("---"))
-            {
-                continue;
-            }
-
-            Debug.Log(savedPathsList[i]);
-
-            // Load xml string from PlayerPrefs
-            string xmlData = PlayerPrefs.GetString(savedPathsList[i]);
-            Debug.Log(xmlData);
-            //yield return StartCoroutine(PostPath(pathNamesSavedList[i], xmlData));
-            yield return StartCoroutine(SaveXml(savedPathsList[i], xmlData));
-            yield return new WaitForEndOfFrame();
-        }
-    }
-
-    public IEnumerator SaveXml(string pathName, string xmlData)
-    {
-        WWWForm wwwForm = new WWWForm();
-        wwwForm.AddField("action", "save");
-        wwwForm.AddField("path_name", UnityWebRequest.EscapeURL(pathName));
-        wwwForm.AddField("xml_file", xmlData);
-        //wwwForm.AddField("timestamp", SystemInfo.deviceUniqueIdentifier); //TimeUtilities.GetUTCUnixTimestamp().ToString());
-                                                                          //wwwForm.AddBinaryData("photo", myPhoto.EncodeToJPG());
-
-        yield return new WaitForEndOfFrame();
-        Texture2D txt2D = Stathis.File_Manager.loadImage<Texture2D>(pathName, Stathis.File_Manager.Ext.JPG);
-
-        if (txt2D)
-        {
-            byte[] bytes = txt2D.EncodeToJPG();
-
-            wwwForm.AddBinaryData("photo", bytes, "hehe.jpg", "image/jpg");
-        }
-
-        yield return new WaitForEndOfFrame();
-
-
-        UnityWebRequest www = UnityWebRequest.Get(getUrl);
-
-        // Wait until the request has been sent and a response from the server been recieved:
-        yield return www;
-
-        if (www.error == null)
-        {
-            // Success!
-            // Debug.Log(www.text);
-        }
-        else
-        {
-            Debug.Log(www.isNetworkError);
-            //pathManager.ShowMessage(www.error, 3f);
-        }
-
-        yield break;
-    }*/
 
     #region InternetConnection
     public void CheckInternet()
@@ -604,7 +502,7 @@ public class ServerManager : MonoBehaviour
         //CreateXMLFile();
     }
 
-    IEnumerator PostJPGFileToDiadrasis()
+    /*IEnumerator PostJPGFileToDiadrasis()
     {
         // Get jpg from resources folder
         Texture2D sunflowerTexture = Resources.Load("Sunflower", typeof(Texture2D)) as Texture2D;
@@ -643,7 +541,7 @@ public class ServerManager : MonoBehaviour
         //yield return new WaitForSeconds(1);
 
         //CreateXMLFile();
-    }
+    }*/
 
     /*IEnumerator GetText()
     {
