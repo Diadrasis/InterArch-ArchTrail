5a89b27cd54ba56f7460e93b243a48a3bc5d9be3
emmanouela88 <76942908+emmanouela88@users.noreply.github.com>
2021-06-15 17:38:52 +0300

    Localization Fixes

--- Assets/LocaleDropdown.cs
+++ Assets/LocaleDropdown.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
 using TMPro;
+using UnityEngine.UI;
 
 public class LocaleDropdown : MonoBehaviour
 {
-    public TMP_Dropdown dropdown;
-
+    public Button buttonUI;
     IEnumerator Start()
     {
         // Wait for the localization system to initialize, loading Locales, preloading etc.
@@ -15,23 +15,40 @@ public class LocaleDropdown : MonoBehaviour
        
 
         // Generate list of available Locales
-        var options = new List<TMP_Dropdown.OptionData>();
+        //var options = new List<TMP_Dropdown.OptionData>();
         int selected = 0;
         for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i)
         {
             var locale = LocalizationSettings.AvailableLocales.Locales[i];
             if (LocalizationSettings.SelectedLocale == locale)
+            {
                 selected = i;
-            options.Add(new TMP_Dropdown.OptionData(locale.name));
-        }
-        dropdown.options = options;
+                
+                if (locale.Identifier.Code == "en")
+                {
+                    buttonUI.GetComponentInChildren<TextMeshProUGUI>().text = locale.Identifier.Code.ToUpper();
+                }
+                else if(locale.Identifier.Code == "el")
+                {
+                    buttonUI.GetComponentInChildren<TextMeshProUGUI>().text = locale.Identifier.Code.ToUpper();
+                }
+                //Debug.Log("Locale identifier = "+locale.Identifier.Code);
+                
+            }
+            
 
-        dropdown.value = selected;
-        dropdown.onValueChanged.AddListener(LocaleSelected);
+            //options.Add(new TMP_Dropdown.OptionData(locale.Identifier.Code.ToUpper()));
+        }
+       // buttonUI.options = options;
+
+        //buttonUI.GetComponentInChildren<TextMeshProUGUI>().text = selected.ToString();
+        buttonUI.onClick.AddListener(()=>LocaleSelected(selected));
     }
 
     static void LocaleSelected(int index)
     {
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+        
+        Debug.Log("Index num: "+index);
     }
 }
