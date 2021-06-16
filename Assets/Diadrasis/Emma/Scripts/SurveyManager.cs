bf68fe285b39495a1e4bcf6c734e783a305cbb4f
emmanouela88 <76942908+emmanouela88@users.noreply.github.com>
2021-06-16 12:55:18 +0300

    small fixes on main with translation

--- Assets/Diadrasis/Emma/Scripts/SurveyManager.cs
+++ Assets/Diadrasis/Emma/Scripts/SurveyManager.cs
@@ -183,7 +183,7 @@ public class SurveyManager : MonoBehaviour
                 demographicOptions[step].SetActive(true);
             }
             
-            if (step==4 ||step == 15 || step == 16 || step == 32 || step == 33 || step == 42 || step == 41 || step == 43 || step == 54 || step == 55 
+            if (step == 4 ||step == 15 || step == 16 || step == 17 || step == 32 || step == 33 || step == 42 || step == 41 || step == 43 || step == 54 || step == 55 
             || step == 69 || step == 72 || step == 73)
             {
                 btnSkip.gameObject.SetActive(false);
