ebb6838b51659f1cbe998848d02799d6647c7b24
Andrew Xeroudakis <a.xeroudakis@gmail.com>
2021-07-07 17:30:38 +0300

    Update

---
+++ Assets/Andrew/Scripts/ToggleSelectLanguage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Localization.Settings;
+
+public class ToggleSelectLanguage : MonoBehaviour
+{
+    public Toggle toggle;
+    public TextMeshProUGUI txtDisplay;
+
+    private void Awake()
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+        txtDisplay.text = "EN";
+    }
+
+    public void ChangeTextDisplay()
+    {
+        if (toggle.isOn)
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+            txtDisplay.text = "EN";
+        }
+        else
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
+            txtDisplay.text = "EL";
+        }
+    }
+}
